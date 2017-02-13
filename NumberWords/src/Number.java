360fc18361b0b39909ad396b62b9560a62c6de62
sujaninekkanti <sujaninekkanti@github.com>
2017-02-13 17:43:22 +0530

    Second Commit..........

--- NumberWords/src/Number.java
+++ NumberWords/src/Number.java
@@ -7,20 +7,20 @@ public class Number {
 	
     public String convertNumberToWords(int n){
     	
-    	if(n<0){
-    		return "Minus  " +  convertNumberToWords(-n);
-    	}
     	if(n<20){
     		return units[n];
     	}
+    	//now the number is 99 less than 100 
     	if(n<100){
     		return tens[n/10] +((n %10!=0) ? " " : "") + units[n%10];
     		
     	}
     	if(n<1000){
+    		//if number is 899 in return statement 899/100 returns quotient 8 +hundred 899%100 is not equal to 0 true adds space then calles the function again 899%100 now returns 99
     		return units[n/100] +" Hundred "+((n %100!=0) ? " " : " ") + convertNumberToWords(n % 100);
     	}
     	if(n<100000){
+    		
     		return convertNumberToWords(n/1000) +" Thousand" +((n%10000!=0) ?" " : " ") + convertNumberToWords(n%1000);
     	} 
     	if(n<10000000){
@@ -35,7 +35,7 @@ public class Number {
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
           Number n = new Number();
-         System.out.println(n.convertNumberToWords(11));
+         System.out.println(n.convertNumberToWords(1134567));
          //System.out.println(n.convertNumberToWords(67));
       }
 
